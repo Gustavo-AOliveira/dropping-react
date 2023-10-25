49c72a6afb05318c983308798e5d96f04516fbe1
GustavoAOliveira <gustavoaquino001@hotmail.com>
2023-10-25 14:05:14 -0300

    Home page pronta

---
+++ src/assets/images.js
@@ -0,0 +1,18 @@
+import logo from "./images/logo-dropping.svg";
+import Jordan from "./images/Jordan.svg";
+import airForce1 from "./images/air-force1.png";
+import nikeP600 from "./images/nike-p-6000.png";
+import nikeWaffle from "./images/nike-waffle.png";
+import nikeESeries from "./images/nike-e-series 1.0.png";
+import airForce07 from "./images/air-force1-07.png";
+import airMaxSystem from "./images/nike-airmax-system.png";
+import facebook from "./images/facebook.png";
+import instagram from "./images/insagram.png";
+import twitter from "./images/twitter.png";
+
+
+
+
+
+
+export { logo, Jordan, airForce1, nikeP600, nikeWaffle, nikeESeries, airForce07, airMaxSystem, facebook, instagram, twitter };
