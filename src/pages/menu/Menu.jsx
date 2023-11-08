730473e3eb7646eb5afda9f8e70642913f928f6c
GustavoAOliveira <gustavoaquino001@hotmail.com>
2023-11-08 15:51:42 -0300

    Bug página menu resolvido

--- src/pages/menu/Menu.jsx
+++ src/pages/menu/Menu.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './styles.module.css';
 
 function ProductManagement() {
   return (
-    <div className={styles.menuContainer}>
-      <div className={styles.header}>
+    <div className={ styles.menuContainer }>
+      <div className={ styles.header }>
         <span>Product management</span>
         <button id="new">Add</button>
       </div>
 
-      <div className={styles.divTable}>
+      <div className={ styles.divTable }>
         <table>
           <thead>
             <tr>
-              <th>ID</th>
+              <th>Code</th>
               <th>Brand</th>
               <th>Name</th>
               <th>Size</th>
@@ -25,6 +25,17 @@ function ProductManagement() {
             </tr>
           </thead>
           <tbody>
+            <tr>
+              <td>1</td>
+              <td>Nike</td>
+              <td>Nike Air Max Plus</td>
+              <td>30</td>
+              <td>Red</td>
+              <td>Pendente</td>
+              <td>Masculino</td>
+              <td><button>Update</button></td>
+              <td><button>Delete</button></td>
+            </tr>
           </tbody>
         </table>
       </div>
